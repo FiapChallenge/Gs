00f92508d99d8e23430ef0706ee2dc2877d4f13f
Asteriuz <gutocebola@gmail.com>
2023-06-03 11:08:04 -0300

    Improved Responsive Landing Page and added all other pages roots

--- Web/js/app.js
+++ Web/js/app.js
@@ -10,4 +10,11 @@ const observer = new IntersectionObserver(entries => {
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
+if (window.innerWidth > 768) {
 hiddenElements.forEach(hiddenElement => observer.observe(hiddenElement));
+}
+
+// remove the class 'hidden' from the elements if window is > 768px
+if (window.innerWidth < 768) {
+    hiddenElements.forEach(hiddenElement => hiddenElement.classList.remove('hidden'));
+}
